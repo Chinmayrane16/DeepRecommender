051667392a80f9a0f12722ba5fb6963df4b3a3d1
Chinmayrane16 <chinmayrane16@gmail.com>
2019-04-03 20:00:18 +0530

    Added Model Architecture

--- model.py
+++ model.py
@@ -7,26 +7,26 @@ from loss import MSEloss_with_Mask
 
 def activation(input, type):
   
-  if type.lower()=='selu':
-    return F.selu(input)
-  elif type.lower()=='elu':
-    return F.elu(input)
-  elif type.lower()=='relu':
-    return F.relu(input)
-  elif type.lower()=='relu6':
-    return F.relu6(input)
-  elif type.lower()=='lrelu':
-    return F.lrelu(input)
-  elif type.lower()=='tanh':
-    return F.tanh(input)
-  elif type.lower()=='sigmoid':
-    return F.sigmoid(input)
-  elif type.lower()=='swish':
-    return F.sigmoid(input)*input
-  elif type.lower()=='identity':
-    return input
-  else:
-    raise ValueError("Unknown non-Linearity Type")
+    if type.lower()=='selu':
+        return F.selu(input)
+    elif type.lower()=='elu':
+        return F.elu(input)
+    elif type.lower()=='relu':
+        return F.relu(input)
+    elif type.lower()=='relu6':
+        return F.relu6(input)
+    elif type.lower()=='lrelu':
+        return F.leaky_relu(input)
+    elif type.lower()=='tanh':
+        return F.tanh(input)
+    elif type.lower()=='sigmoid':
+        return F.sigmoid(input)
+    elif type.lower()=='swish':
+        return F.sigmoid(input)*input
+    elif type.lower()=='identity':
+        return input
+    else:
+        raise ValueError("Unknown non-Linearity Type")
 
 
 class AutoEncoder(nn.Module):
@@ -40,8 +40,8 @@ class AutoEncoder(nn.Module):
         
         nl_type = non-Linearity type (default: 'selu).
         is_constrained = If true then the weights of encoder and decoder are tied.
-        dp_drop_prob = Dropout probability
-        last_layer_activations = Whether to apply activation on last decoder layer
+        dp_drop_prob = Dropout probability.
+        last_layer_activations = Whether to apply activation on last decoder layer.
         """
 
         super(AutoEncoder, self).__init__()
@@ -51,4 +51,65 @@ class AutoEncoder(nn.Module):
         self.is_constrained = is_constrained
         self.dp_drop_prob = dp_drop_prob
         self.last_layer_activations = last_layer_activations
+
+        if dp_drop_prob>0:
+            self.drop = nn.Dropout(dp_drop_prob)
+
+        self._last = len(layer_sizes) - 2
+
+        # Initaialize Weights
+        self.encoder_weights = nn.ParameterList( [nn.Parameter(torch.rand(layer_sizes[i+1], layer_sizes[i])) for i in range(len(layer_sizes)) - 1 ] )
+
+        # "Xavier Initialization" ( Understanding the Difficulty in training deep feed forward neural networks - by Glorot, X. & Bengio, Y. )
+        # ( Values are sampled from uniform distribution )
+        for weights in self.encoder_weights:
+            init.xavier_uniform_(weights)
+
+        # Encoder Bias
+        self.encoder_bias = nn.ParameterList( [nn.Parameter(torch.zeros(layer_sizes[i+1])) for i in range(len(layer_sizes)) - 1] )
+
+        reverse_layer_sizes = list(reversed(layer_sizes)) 
+        # reversed returns iterator
+
+
+        # Decoder Weights
+        if is_constrained == False:
+            self.decoder_weights = nn.ParameterList( [nn.Parameter(torch.rand(reverse_layer_sizes[i+1], reverse_layer_sizes[i])) for i in range(len(reverse_layer_sizes)) - 1] )
+
+            for weights in self.decoder_weights:
+                init.xavier_uniform_(weights)
+
+        self.decoder_bias = nn.ParameterList( [nn.Parameter(torch.zeros(reverse_layer_sizes[i+1])) for i in range(len(reverse_layer_sizes)) - 1] )
+
+
+
+    def encode(self,x):
+        for i,w in enumerate(self.encoder_weights):
+            x = F.linear(input=x, weight = w, bias = self.encoder_bias[i] )
+            x = activation(input=x, type=self.nl_type)
+
+        # Apply Dropout on the last layer
+        if self.dp_drop_prob > 0:
+            x = self.drop(x)
+
+        return x
+
+
+    def decode(self,x):
+        if self.is_constrained == True:
+            # Weights are tied
+            for i,w in zip(range(len(self.encoder_weights)),list(reversed(self.encoder_weights))):
+                x = F.linear(input=x, weight=w.t(), bias = self.decoder_bias[i] )
+                x = activation(input=x, type=self.nl_type if i != self._last or self.last_layer_activations else 'identity')
+
+        else:
+
+            for i,w in enumerate(self.decoder_weights):
+                x = F.linear(input=x, weight = w, bias = self.decoder_weights[i])
+                x = activation(input=x, type=self.nl_type if i != self._last or self.last_layer_activations else 'identity')
+
+        return x
+
+    def forward(self,x):
+        return self.decode(self.encode(x))
 
