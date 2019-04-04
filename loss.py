d80bf5062c893a845342e7254a5de3f04118e850
Chinmayrane16 <chinmayrane16@gmail.com>
2019-04-04 19:19:00 +0530

    Added MMSE loss

--- loss.py
+++ loss.py
@@ -5,13 +5,13 @@ from torch.autograd import Variable
 
 def MSEloss_with_Mask(inputs, target):
     # Masking into a vector of 1's and 0's.
-  mask = [int(i) for i in target]
+  mask = [float(i!=0.0) for i in target]
   mask = torch.tensor(mask)
 
   # Actual number of ratings.
   number_ratings = torch.sum(mask)
   # To avoid division by zero while calculating loss.
-  number_ratings = 1 if number_ratings == 0 else number_ratings.float()
+  number_ratings = 1.0 if number_ratings == 0.0 else number_ratings
   
   error = torch.sum(torch.mul(mask,torch.mul((target-inputs),(target-inputs))))
   loss = error.div(number_ratings)
