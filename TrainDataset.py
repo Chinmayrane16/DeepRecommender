780050e0fa60028b27fb6825ddf0da4c7a6a6a9b
Chinmayrane16 <chinmayrane16@gmail.com>
2019-04-05 21:30:22 +0530

    Added Train Dataset Class

---
+++ TrainDataset.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import matplotlib.pyplot as plt
+
+class TrainDataset(Dataset):
+    def __init__(self, train_file, transform=None):
+        self.data = pd.read_csv(train_file)
+        self.data = self.data.iloc[:,1:]
+        self.transform = transform
+        
+        if transform is not None:
+            self.data = self.transform(np.array(self.data))
+        
+    def __len__(self):
+        return len(self.data[0])
+    
+    def __getitem__(self, ind):
+        user_vector = self.data.data[0][ind]
+        
+        return user_vector
